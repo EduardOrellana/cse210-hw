4aa791a23da86641e17897ae96dda3ffd1d9e565
EduardOrellana <ore23006@byui.edu>
2024-04-27 09:00:25 -0600

    last program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Hello World");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your number: ");
+        string numberFromUser = Console.ReadLine();
+        int numberUser = int.Parse(numberFromUser);
+
+        return numberUser;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int Square = number * number;
+        return Square;
+    }
+
+    static void DisplayResult(int number, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+
+        string userName = PromptUserName();
+        int userNUmber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNUmber);
+
+        DisplayResult(squareNumber, userName);
+
     }
 }
