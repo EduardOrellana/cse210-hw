43e64e21f92994b7c6a2619d9f2e9fc744d6cadf
EduardOrellana <ore23006@byui.edu>
2024-05-01 22:39:02 -0600

    Team Activity Week 02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+
+
+
     }
 }
