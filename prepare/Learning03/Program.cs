2a633a2538e5388347fed46a6e83495181d26fff
EduardOrellana <ore23006@byui.edu>
2024-05-08 17:57:20 -0600

    prepare activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -10,20 +10,21 @@ class Program
 
 
         
-    Fraction fractionOne = new Fraction();
-    Fraction fractionTwo = new Fraction(5);
-    Fraction fractionThree = new Fraction(3, 4);
-    Fraction fractionFour = new Fraction(1, 3);
-    // Fraction fractionFive = new Fraction();
-    // Fraction fractionSix = new Fraction();
-    // Fraction fractionSeven = new Fraction();
-    // Fraction fractionEight = new Fraction();
-    // Fraction fractionNine = new Fraction();
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
 
-    Console.WriteLine(fractionOne.GetDecimalValue());
-    Console.WriteLine(fractionTwo.GetDecimalValue());
-    Console.WriteLine(fractionThree.GetDecimalValue());
-    Console.WriteLine(fractionFour.GetDecimalValue());
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     
     
     }
