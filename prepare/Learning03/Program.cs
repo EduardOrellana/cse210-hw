9adfa0d0c8543e934877285223ad247a399e02d0
EduardOrellana <ore23006@byui.edu>
2024-05-08 18:01:46 -0600

    p:

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,9 +7,6 @@ class Program
     {
         Console.WriteLine("Hello Learning03 World!");
 
-
-
-        
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
