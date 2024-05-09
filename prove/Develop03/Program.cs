de2602f637ac304a14a4fa54df3435dc92712676
EduardOrellana <ore23006@byui.edu>
2024-05-09 17:59:33 -0600

    Moving the second branch to the first branch

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,18 +2,48 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Develop03 World!");
+        
+        string toQuite = "";
 
+        Reference reference= new Reference("Proverbs", 3, 5, 6);
 
+        string scriptureText = @"Trust in the Lord with all thine heart; and lean not unto thine own understanding.
 
+        In all thy ways acknowledge him, and he shall direct thy paths.";
 
+        Scripture scripture= new Scripture(reference, scriptureText);
 
+        do
+        {
+            Console.WriteLine();
+            Console.WriteLine(scripture.GetDisplayText());
+            
+            Console.WriteLine("\nPress enter to continue or type 'quite' to finish");
 
+            toQuite = Console.ReadLine();
+            Console.Clear();
+
+            if (toQuite != "quite")
+            {
+                scripture.HideRandomWords(1);
+            }
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("try to memorize!");
+                break;
+            }
+    
+        }while (toQuite != "quite");
 
     }
 }
